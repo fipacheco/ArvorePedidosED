062e03998ffa02af14468c4c70a316b56b5bbe1e
fipacheco <filipesmpacheco@gmail.com>
2023-06-11 19:15:08 -0300

    classe adicional de correcao

---
+++ src/ResultadoRemocao.java
@@ -0,0 +1,11 @@
+public class ResultadoRemocao {
+    private boolean removido;
+
+    public boolean isRemovido() {
+        return removido;
+    }
+
+    public void setRemovido(boolean removido) {
+        this.removido = removido;
+    }
+}
